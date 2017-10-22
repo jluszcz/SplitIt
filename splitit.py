dfe419f9dc9d3d221b50575c191117bbe9c2bc3b
Jacob Luszcz <jacob.luszcz@gmail.com>
2017-10-22 12:40:50 -0400

    Implement more location management in splitit
    - Create initial swagger definition
    - Add list of how to test
    - More location management in splitit

--- splitit.py
+++ splitit.py
@@ -42,6 +42,59 @@ def create_check(date, name):
 def get_check(date, name):
     return _get_check(date, name)
 
+def _get_location_by_name(check, location):
+    if 'locations' in check:
+        for loc in check['locations']:
+            if not location and loc['id'] == DEFAULT_LOCATION_ID:
+                return loc
+            if loc.get('name') == location:
+                return loc
+
+    return None
+
+def _get_next_location_id(check):
+    if 'locations' not in check:
+        return DEFAULT_LOCATION_ID + 1
+
+    max_id = -1
+    for loc in check['locations']:
+        max_id = max(max_id, loc['id'])
+    return max_id + 1
+
+def update_location(date, name, location, data):
+    check = _get_check(date, name)
+    if not check:
+        return {}
+
+    loc = _get_location_by_name(check, location)
+
+    if not loc:
+        # Create the default location if necessary
+        if not location:
+            loc = {
+                'id': DEFAULT_LOCATION_ID
+            }
+        else:
+            loc = {
+                'id': _get_next_location_id(check),
+                'name': location
+            }
+
+        # Store the location in the check
+        if 'locations' not in check:
+            check['locations'] = []
+
+        check['locations'].append(loc)
+
+    if 'tax' in data:
+        loc['tax'] = data['tax']
+    if 'tip' in data:
+        loc['tip'] = data['tip']
+
+    _save_check(check)
+
+    return loc
+
 def get_check_grouped_by_owner(date, name):
     check = _get_check(date, name)
     if not check:
--- splitit_api.py
+++ splitit_api.py
@@ -13,6 +13,26 @@ def get_check(date, name):
         flask.abort(404)
     return flask.jsonify(check)
 
+@app.route('/checks/<date>/<name>', methods=['PUT'])
+def create_check(date, name):
+    try:
+        return flask.jsonify(splitit.create_check(date, name))
+    except ValueError:
+        flask.abort(409)
+
+@app.route('/checks/<date>/<name>/locations')
+def get_check_locations(date, name):
+    check = splitit.get_check(date, name)
+    if not check:
+        flask.abort(404)
+    return flask.jsonify(check.get('locations', []))
+
+@app.route('/checks/<date>/<name>/location/<location>', methods=['POST'])
+def update_location(date, name, location=None):
+    data = flask.request.get_json()
+    loc_data = splitit.update_location(date, name, location, data)
+    return flask.jsonify(loc_data or [])
+
 @app.route('/checks/<date>/<name>/by_owner')
 def get_amounts_by_owner(date, name):
     amounts = splitit.get_check_grouped_by_owner(date, name)
